405ab7f54ac44918f3db27fb4019462ed5caa740
Labinko_O <Labinko_O@Labinko_O.iba>
2017-12-08 13:32:29 +0300

    first c
    ffff

---
+++ src/myPR801/myClass.java
@@ -0,0 +1,8 @@
+package myPR801;
+
+public class myClass {
+	  public static void main(String[] args) {
+	        // Display "Hello World! oooo"
+	    	System.out.println("Hello World! ooooo");
+	    }
+}
